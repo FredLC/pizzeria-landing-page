3b50e095b284d633d2118494c7d75b250eaf6025
fredlc <fredlefevre@outlook.com>
2020-04-29 16:14:36 -0400

    Created header and footer

---
+++ src/components/Header-Footer/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <footer>
+        <div class="container">
+            <div>
+                <img :src="logoIcon" alt="footer logo" />
+                <div class="contact">
+                    <div>Call Us</div>
+                    <div>555-555-5555</div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+import Logo from '../../assets/images/logo.png';
+
+export default {
+    data() {
+        return {
+            logoIcon: Logo,
+        };
+    },
+};
+</script>
