0024fa49c223758b668e7c6313455dadabfe6f6d
fredlc <fredlefevre@outlook.com>
2020-04-29 18:26:03 -0400

    Included vue-scroll-reveal library

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import VueResource from 'vue-resource';
+import VueScrollReveal from 'vue-scroll-reveal';
 
 import { MdButton, MdDrawer, MdCard } from 'vue-material/dist/components';
 import 'vue-material/dist/vue-material.min.css';
@@ -8,7 +9,14 @@ import 'vue-material/dist/vue-material.min.css';
 Vue.use(MdButton);
 Vue.use(MdDrawer);
 Vue.use(MdCard);
+
 Vue.use(VueResource);
+
+Vue.use(VueScrollReveal, {
+    duration: 800,
+    scale: 1,
+    distance: '50px',
+});
 
 Vue.http.options.root = process.env.VUE_APP_URL;
 
